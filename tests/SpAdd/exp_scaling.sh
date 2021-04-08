d23e7f131fc306f0b0c339f604d84a0d16c0d723
Taufique Hussain <mth@indiana.edu>
2021-04-08 09:06:22 -0400

    Committing changes made in the process of experiment
    - MKL tree style add
    - Pairwise tree style add
    - Multi add with spa
    - Improved multi add with heap

--- tests/SpAdd/exp_scaling.sh
+++ tests/SpAdd/exp_scaling.sh
@@ -2,15 +2,26 @@
 
 #SBATCH -p azad
 #SBATCH -N 1
-#SBATCH -t 03:00:00
+#SBATCH -t 10:00:00
 #SBATCH -J spadd
 #SBATCH -o spadd.o%j
 
 export OMP_NUM_THREADS=48
-export FILE=hash.csv
-export BIN=./binary
+
+#export FILE=hash.csv
+#export FILE=heap.csv
+export FILE=mkl.csv
+#export FILE=dense.csv
+#export FILE=pairwise.csv
+#export FILE=window.csv
+
+#export BIN=./binary
+#export BIN=./heap
+export BIN=./mkl
 
 echo "matrix,scale,d,k,thread,algorithm,total" > $FILE
+#echo "matrix,scale,d,k,thread,algorithm,threshold,count,time" > $FILE
+#echo "matrix,scale,d,k,thread,algorithm,window,symbolic,computation,total" > $FILE
 
 $BIN 16 8 10 0 >> $FILE
 $BIN 16 8 20 0 >> $FILE
@@ -61,3 +72,6 @@ $BIN 20 32 10 1 >> $FILE
 $BIN 20 32 20 1 >> $FILE
 $BIN 20 32 40 1 >> $FILE
 $BIN 20 32 80 1 >> $FILE
+
+#$BIN 20 64 40 0 >> $FILE
+#$BIN 20 128 40 0 >> $FILE
